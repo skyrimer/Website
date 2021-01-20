ae3d4ebc393ae7b6801c57b3532a709f299d3012
Kirill <chekmenev2004@mail.ru>
2021-01-20 16:11:00 +0300

    odd file delete

--- push_to_git.py
+++
@@ -1,6 +0,0 @@
-from subprocess import call
-call('git init')
-call("git add -A")
-call('git commit -m "commit for git"')
-call("git remote add origin https://github.com/skyrimer/Website")
-call("git push -u origin master")
