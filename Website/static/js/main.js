cdb8d8dd5f8995819a39cd0f9e37c02a0e3663e8
Kirill <chekmenev2004@mail.ru>
2021-01-27 11:12:30 +0300

    css changes for themes

--- Website/static/js/main.js
+++ Website/static/js/main.js
@@ -1,3 +1,4 @@
+//prepoader of the page
 window.onload = function () {
   document.body.classList.add('loaded_hiding');
   window.setTimeout(function () {
@@ -5,19 +6,39 @@ window.onload = function () {
     document.body.classList.remove('loaded_hiding');
   }, 500);
 }
-  (function () {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
 
+// Custum validation with Bootstrap
+var forms = document.querySelectorAll('.needs-validation')
+Array.prototype.slice.call(forms)
+  .forEach(function (form) {
+    form.addEventListener('submit', function (event) {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+      form.classList.add('was-validated')
+    }, false)
+  })
+
+// Switch theme
+let switchTheme = document.getElementById('switchTheme')
+build_path = (...args) => {
+  return args.map((part, i) => {
+    if (i === 0) {
+      return part.trim().replace(/[\/]*$/g, '')
+    } else {
+      return part.trim().replace(/(^[\/]*|[\/]*$)/g, '')
+    }
+  }).filter(x=>x.length).join('/')
+}
+
+switchTheme.onclick = function () {
+  let theme = document.getElementById('theme')
+  let lightTheme = "/static/css/main.css"
+
+  if (theme.getAttribute('href') == lightTheme ) {
+    theme.href = "/static/css/main-dark.css";
+  } else {
+    theme.href = lightTheme;
+  }
+}
